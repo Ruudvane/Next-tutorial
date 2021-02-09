4bc27179a695800be134ac39247ce20b35107cb1
Ruud <ruudvane@gmail.com>
2021-02-09 20:38:21 +0100

    Adjusted Footer

--- comps/Footer.js
+++ comps/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             <p>Copyright 2021 - Ruud van Eldik</p>
-            <p>Created with NextJS - Hosted by vercel.com</p>
+            <p>Created with NextJS - Hosted by <bold>vercel.com</bold></p>
         </footer>
     );
 }
